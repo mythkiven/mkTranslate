872a0c579a154da9198021c74db866bfd834d8b1
jiangxiaocai <jiangxiaocai@cryptogorilla.io>
2019-06-25 09:24:46 +0800

    Update document

--- setup.py
+++ setup.py
@@ -29,34 +29,16 @@ def get_description():
     return description
 
 
-# def get_readme():
-#     return get_file(os.path.dirname(__file__), 'README.rst')
-
 
 def install():
     setup(
         name='mkTranslation',
         version=get_version(),
         description=get_description(),
-        # long_description=get_readme(),
         license='MIT',
         author='MythKiven',
         author_email='mythkiven' '@' 'qq.com',
         url='https://github.com/mythkiven/mkTranslate',
-        # classifiers=['Development Status :: 5 - Production/Stable',
-        #              'Intended Audience :: Education',
-        #              'Intended Audience :: End Users/Desktop',
-        #              'License :: Freeware',
-        #              'Operating System :: POSIX',
-        #              'Operating System :: Microsoft :: Windows',
-        #              'Operating System :: MacOS :: MacOS X',
-        #              'Topic :: Education',
-        #              'Programming Language :: Python',
-        #              'Programming Language :: Python :: 2.7',
-        #              'Programming Language :: Python :: 3.4',
-        #              'Programming Language :: Python :: 3.5',
-        #              'Programming Language :: Python :: 3.6',
-        #              'Programming Language :: Python :: 3.7'],
         packages=find_packages(exclude=['test']),
         keywords='google translate translator',
         install_requires=[
